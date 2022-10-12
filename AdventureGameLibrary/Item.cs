080f6927a39b19267a780847de6c2a2f32a48a1e
loicp <loic.polain@student.ehb.be>
2022-10-12 11:40:36 +0200

    feat: init classes and interfaces

---
+++ AdventureGameLibrary/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureGameLibrary
+{
+    internal class Item
+    {
+    }
+}
---
+++ AdventureGameLibrary/Room.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventureGameLibrary
+{
+    public class Room
+    {
+    }
+}
